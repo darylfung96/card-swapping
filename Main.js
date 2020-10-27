ae06b2d10e333d44150088c7fd011e43b5499ee1
Daryl Fung <darylfung96@gmail.com>
2020-10-26 22:53:02 -0500

    added common jsg

--- Main.js
+++ Main.js
@@ -42,22 +42,6 @@ function getUrlParameter(name) {
 }
 
 /**
- * This function creates a random string
- *
- * @param {int} length - the length of the randomize string
- */
-function createRandomString(length) {
-  var result = '';
-  var characters =
-    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  var charactersLength = characters.length;
-  for (var i = 0; i < length; i++) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-  }
-  return result;
-}
-
-/**
  * this function returns to main menu
  */
 Main.prototype.returnMenuCallback = function () {
---
+++ components/common.js
@@ -0,0 +1,15 @@
+/**
+ * This function creates a random string
+ *
+ * @param {int} length - the length of the randomize string
+ */
+function createRandomString(length) {
+  var result = '';
+  var characters =
+    'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
